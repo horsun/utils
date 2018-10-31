8ace65cf406cbd53e8cc9c7c8aaac16be1a4dc2d
jinhao <im7h1995@hotmail.com>
2018-10-31 16:16:36 +0800

    django rest frame 分页

--- drf_pagination.py
+++ drf_pagination.py
@@ -3,8 +3,14 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
+# Usage
+#       在接口中添加下面代码
+#     pagination_class = AppPagination
+# 或者在setting中的 drf设置里面添加
+#      'DEFAULT_PAGINATION_CLASS': '你的目录.AppPagination'
+# url：    X.X.X.X/books/?page=1&pageSize=10
 class AppPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 10  # 如果url parm 没有pageSize 参数 就默认为10
     page_query_param = 'page'
     page_size_query_description = 'pageSize'
     page_size_query_param = 'pageSize'
