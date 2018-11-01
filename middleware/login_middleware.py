d114c49d4cbddfa171cabc96f837564543e2692d
jinhao <im7h1995@hotmail.com>
2018-11-01 13:22:53 +0800

    jwt失效方法实现 fix 登录失败

--- middleware/login_middleware.py
+++ middleware/login_middleware.py
@@ -3,9 +3,10 @@ from uuid import uuid4
 from django.http import HttpResponse
 from jwt import InvalidSignatureError
 from rest_framework.exceptions import ValidationError
+from rest_framework.status import is_success
 from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
 
-# 详细讲解见 https://blog.csdn.net/qq_33042187/article/details/83347337
+
 # 1.每次登录 response 处理 记录 jwt
 # 2.每次请求判断 jwt是否与表中相等(相当于 用户 异设备登录获取了新的jwt)  不等 就修改uuid
 
@@ -13,7 +14,7 @@ from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
 class ValidTokenMiddleware(object):
     def process_request(self, request):
         jwt_token = request.META.get('HTTP_AUTHORIZATION', None)
-        if jwt_token != '' and jwt_token is not None:
+        if jwt_token:
             data = {
                 'token': request.META['HTTP_AUTHORIZATION'].split(' ')[1],
             }
@@ -30,11 +31,14 @@ class ValidTokenMiddleware(object):
 
     def process_response(self, request, response):
         if request.META['PATH_INFO'] == '/login/':
-            rep_data = response.data
-            valid_data = VerifyJSONWebTokenSerializer().validate(rep_data)
-            user = valid_data['user']
-            user.user_jwt = rep_data['token']
-            user.save()
-            return response
+            if is_success(response.status_code):
+                rep_data = response.data
+                valid_data = VerifyJSONWebTokenSerializer().validate(rep_data)
+                user = valid_data['user']
+                user.user_jwt = rep_data['token']
+                user.save()
+                return response
+            else:
+                return response
         else:
             return response
