0fa414a92444e9fc540ebb97ed4b0affec1577b5
Horsun <im7h1995@Hotmail.com>
2018-12-11 15:13:28 +0800

    Update error_middleware.py

--- middleware/error_middleware.py
+++ middleware/error_middleware.py
@@ -1,7 +1,65 @@
-from Lyonline.settings import error_logger
+from settings import error_logger
 
 
 class ExceptionLoggingMiddleware(object):
     def process_exception(self, request, exception):
         import traceback
         error_logger.error(traceback.format_exc())
+
+        
+#  setting.py logging-------
+import logging
+import django.utils.log
+import logging.handlers
+
+error_logger = logging.getLogger('django.error')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s',
+        }
+        # 日志格式
+    },
+    'filters': {
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': './django_all.log',  # 日志输出文件
+            'maxBytes': 1024 * 1024 * 5,  # 文件大小
+            'backupCount': 5,  # 备份份数
+            'formatter': 'standard',  # 使用哪种formatters日志格式
+        },
+        'error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': './django_error.log',
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        # 设置 getlogger()
+        'django': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django.error': {
+            'handlers': ['error'],
+            'level': 'ERROR',
+            'propagate': True
+        },
+
+    }
+}
